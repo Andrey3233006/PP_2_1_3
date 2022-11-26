03178c3c0ab1398b13ad32cda20556333be839bd
Andrey3233006 <111655609+Andrey3233006@users.noreply.github.com>
2022-11-26 16:23:11 +0300

    Add files via upload

---
+++ src/main/java/App.java
@@ -0,0 +1,26 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        System.out.println("Cравнение бинов helloworld: " + (bean == bean1));
+
+        Cat bean2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean2.getCatSay());
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getCatSay());
+        System.out.println("Cравнение бинов cat: " + (bean2 == bean3));
+    }
+
+}
---
+++ src/main/java/Cat.java
@@ -0,0 +1,10 @@
+public class Cat {
+    private String catSay;
+    public Cat() {
+        this.catSay = catSay;
+    }
+
+    public String getCatSay() {
+        return catSay;
+    }
+}
